c4802cdb9de433411f6b81e7505926d450e42816
brunosag <brunosag02@gmail.com>
2023-10-29 17:32:24 -0300

    chore: add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,18 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        white: '#FFF',
+        primary: '#603B88',
+        secondary: '#B22559',
+        grey: '#353535',
+        blue: '#3B6388',
+        green: '#3B8863',
+        red: '#AD3F3F',
       },
     },
   },
   plugins: [],
-}
+};
