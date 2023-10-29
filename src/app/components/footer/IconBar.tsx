765d6963e0dc7d7a9f72e3c898ec5f8287525f97
yasmin beer <55171112+beersprite@users.noreply.github.com>
2023-10-29 17:04:09 -0300

    hover effect

--- src/app/components/footer/IconBar.tsx
+++ src/app/components/footer/IconBar.tsx
@@ -4,20 +4,21 @@ import InstaLogo from '../../../../public/img/logos/instagram-logo.png';
 import IDELogo from '../../../../public/img/logos/ide-logo.png';
 import LinkedinLogo from '../../../../public/img/logos/linkedin-logo.png';
 import Image from "next/image";
+import '../../styles/animation.css';
 
 export default function IconBar() {
     return (
         <div className="flex gap-x-5">
-            <a href="https://google.com/" className="w-8">
+            <a href="https://google.com/" className="w-8 hover-scale">
                 <Image src={PokemonLogo} alt="Pokémon logo"/>
             </a>
-            <a href="https://google.com/" className="w-8">
+            <a href="https://google.com/" className="w-8 hover-scale">
                 <Image src={InstaLogo} alt="Instagram logo"/>
             </a>
-            <a href="https://google.com/" className="w-8">
+            <a href="https://google.com/" className="w-8 hover-scale">
                 <Image src={LinkedinLogo} alt="Linkedin logo"/>
             </a>
-            <a href="https://google.com/" className="w-14">
+            <a href="https://google.com/" className="w-14 hover-scale">
                 <Image src={IDELogo} alt="IDE logo"/>
             </a>
         </div>
