62c7266b07ed83d458dbbaca8690f1cb9126662a
yasmin beer <55171112+beersprite@users.noreply.github.com>
2023-10-29 14:48:01 -0300

    footer looking like figma

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -6,21 +6,19 @@ import '../../styles/font.css';
 
 export default function Footer() {
     return (
-        <footer className="pt-10 bg-zinc-800">
-            <div className="max-w-screen-xl mx-auto px-4 md:px-8">
-                <div className="justify-between items-center gap-12 md:flex">
-                    <div className="flex-1 max-w-lg pixel-font">
-                        <p>
+        <footer className="p-14 bg-zinc-800">
+            <div className="max-w-screen-xl mx-auto flex flex-col items-center justify-between">
+                    <IconBar/>
+                    <div className="pixel-font text-center">
+                        <p className="mt-5">
                             株式会社ポケモン (The Pokémon Company) © x IDE © Todos os direitos reservados.
                         </p>
-                        <p>
+                        <p className="mt-3">
                             Termos de Uso | Política de Privacidade | Política de Cookies
                         </p>
-                    </div>
                 </div>
             </div>
-            <div className="mt-10 py-10 items-center justify-between sm:flex">
-                <IconBar/>
+            <div className="mt-10 justify-end sm:flex">
                 <a href="https://google.com/">
                     <Image src={IDEPixelLogo} alt="IDE logo"/>
                 </a>
