35fdf9ad19b4210f7635830e2d3ef1916243b1eb
yasmin beer <55171112+beersprite@users.noreply.github.com>
2023-10-29 17:19:02 -0300

    added links

--- src/app/components/footer/IconBar.js
+++ src/app/components/footer/IconBar.js
@@ -9,16 +9,16 @@ import '../../styles/animation.css';
 export default function IconBar() {
     return (
         <div className="flex gap-x-5">
-            <a href="https://google.com/" className="w-8 hover-scale">
+            <a href="https://www.pokemon.com/br/" className="w-8 hover-scale">
                 <Image src={PokemonLogo} alt="Pokémon logo"/>
             </a>
-            <a href="https://google.com/" className="w-8 hover-scale">
+            <a href="https://www.instagram.com/ide_jr/" className="w-8 hover-scale">
                 <Image src={InstaLogo} alt="Instagram logo"/>
             </a>
-            <a href="https://google.com/" className="w-8 hover-scale">
+            <a href="https://www.linkedin.com/company/empresa-j%C3%BAnior-ide/mycompany/" className="w-8 hover-scale">
                 <Image src={LinkedinLogo} alt="Linkedin logo"/>
             </a>
-            <a href="https://google.com/" className="w-14 hover-scale">
+            <a href="https://www.idejr.com.br/" className="w-14 hover-scale">
                 <Image src={IDELogo} alt="IDE logo"/>
             </a>
         </div>
