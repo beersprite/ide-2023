e547e8603b9e402aea5fd2e31ce0ce2b776e2e17
yasmin beer <55171112+beersprite@users.noreply.github.com>
2023-10-29 11:52:11 -0300

    starting footer component

---
+++ src/app/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer() {
+        return (
+        <footer className="text-gray-500 bg-white px-4 py-5 max-w-screen-xl mx-auto md:px-8">
+            <span>hi</span>
+        </footer>
+    )
+}
