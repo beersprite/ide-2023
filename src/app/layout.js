f85d3d5c7fda2f842fc4de6e3489eac9186aa7ef
Ricardo Zanini De Costa <ricardozaninidecosta@gmail.com>
2023-10-30 09:52:16 -0300

    Resolved merge conflict p-2

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,11 +2,7 @@ import { Press_Start_2P } from 'next/font/google';
 import localFont from 'next/font/local';
 
 import './globals.css';
-<<<<<<< HEAD
-import Footer from './components/footer/Footer';
-import Starters from './components/starters/Starters';
-import Sobre from './components/sobre/Sobre';
-=======
+
 
 const pixelFont = localFont({ src: '../../public/fonts/uni05_53.ttf' });
 
@@ -15,7 +11,6 @@ const pressStart2P = Press_Start_2P({
   preload: false,
   variable: '--font-title',
 });
->>>>>>> 9c5115c0bfeb1f867450c7fcc985e0bfd84b19d8
 
 export const metadata = {
   title: 'PokéIDEx',
@@ -24,16 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-<<<<<<< HEAD
-      <body>
-        {children}
-        <Starters/>
-        <Sobre/>
-        <Footer />
-      </body>
-=======
       <body className={`${pixelFont.className} ${pressStart2P.variable}`}>{children}</body>
->>>>>>> 9c5115c0bfeb1f867450c7fcc985e0bfd84b19d8
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Footer from './components/footer/Footer';
 import Inicio from './components/Inicio';
+import Starters from './components/starters/Starters';
+import Sobre from './components/sobre/Sobre';
 
 export default function Home() {
   return (
     <>
       <Inicio />
+      <Starters/>
+      <Sobre/>
       <Footer />
     </>
   );
