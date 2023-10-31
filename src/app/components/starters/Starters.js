bb433fa154a1e68b5d472a4c13767e6898de5501
brunosag <brunosag02@gmail.com>
2023-10-31 05:50:53 -0300

    fix: `gatoDormindo.svg` import

--- src/app/components/starters/Starters.js
+++ src/app/components/starters/Starters.js
@@ -1,20 +1,18 @@
-
 import Image from 'next/image';
 import React from 'react';
 import BotoesElementos from './BotoesElementos.js';
 
 import './starters.css';
-import GatoDormindo from '../../icons/Starters/GatoDormindo.svg';
+import GatoDormindo from '../../icons/Starters/gatoDormindo.svg';
 import Aspas from '../../icons/Starters/aspas.svg';
 import logoIDE from '../../icons/logoIDE.svg';
 
 export default function Starters() {
   return (
     <div className="pt-40 pb-40 text-primary">
-        <div className="block text-center text-6xl mb-20">
-            CONHECENDO OS STARTERS
-        </div>
-        <div className="
+      <div className="block text-center text-6xl mb-20">CONHECENDO OS STARTERS</div>
+      <div
+        className="
             mx-10 
             mb-5
             border-primary
@@ -24,8 +22,10 @@ export default function Starters() {
             flex 
             flex-wrap
             containerConteudo
-        ">
-            <div className="
+        "
+      >
+        <div
+          className="
                 border-primary 
                 border-block 
                 w-5/12
@@ -35,29 +35,38 @@ export default function Starters() {
                 flex-col 
                 items-center
                 p-10
-            ">
-                <div className="text-2xl pb-5">ESCOLHA UM!</div>
-                <Image className="w-48" src={GatoDormindo} alt="Gatinho Dormindo"/>
-            </div>
-            <div className="
+            "
+        >
+          <div className="text-2xl pb-5">ESCOLHA UM!</div>
+          <Image className="w-48" src={GatoDormindo} alt="Gatinho Dormindo" />
+        </div>
+        <div
+          className="
                 flex 
                 flex-col 
                 items-center
                 p-10
                 w-7/12
-            ">
-                <div className="w-full flex justify-start pb-2">
-                    <Image className="w-10" src={Aspas} alt="Aspas" width="87" />
-                </div>
-                <div className="px-5 text-0_5xl text-center">Treinadores Pokémon enfrentam uma decisão crucial ao escolher seu Pokémon inicial, pois isso moldará suas aventuras. Os tipos - Água, Fogo e Planta - influenciam as batalhas e estratégias. Água domina Fogo, Fogo vence Planta, e Planta supera Água. Escolher com sabedoria é a chave para o sucesso nas jornadas Pokémon. Conheça um pouco mais sobre esses tipos!</div>
-                <div className="w-full flex justify-end pb-2">
-                    <Image className="imagemEspelhada w-10" src={Aspas} alt="Aspas" width="87" />
-                </div>
-            </div>
+            "
+        >
+          <div className="w-full flex justify-start pb-2">
+            <Image className="w-10" src={Aspas} alt="Aspas" width="87" />
+          </div>
+          <div className="px-5 text-0_5xl text-center">
+            Treinadores Pokémon enfrentam uma decisão crucial ao escolher seu Pokémon inicial, pois isso moldará suas
+            aventuras. Os tipos - Água, Fogo e Planta - influenciam as batalhas e estratégias. Água domina Fogo, Fogo
+            vence Planta, e Planta supera Água. Escolher com sabedoria é a chave para o sucesso nas jornadas Pokémon.
+            Conheça um pouco mais sobre esses tipos!
+          </div>
+          <div className="w-full flex justify-end pb-2">
+            <Image className="imagemEspelhada w-10" src={Aspas} alt="Aspas" width="87" />
+          </div>
         </div>
-        <div className="flex justify-between mx-10 mb-5">
-            <BotoesElementos/>
-            <div className="
+      </div>
+      <div className="flex justify-between mx-10 mb-5">
+        <BotoesElementos />
+        <div
+          className="
                 w-7/12
                 border-primary
                 border-solid 
@@ -67,10 +76,11 @@ export default function Starters() {
                 justify-center
                 cursor-pointer
                 cursor-pointer
-            ">
-                <Image className="w-16" src={logoIDE} alt="IDE logo" />
-            </div>
+            "
+        >
+          <Image className="w-16" src={logoIDE} alt="IDE logo" />
         </div>
+      </div>
     </div>
   );
 }
